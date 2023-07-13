135abbb761aa1e2b73dd8daea6faa59c4d1d9db3
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-07-13 12:49:55 -0400

    Mostly done w reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,28 @@
+const { Schema } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            // Use Mongoose's ObjectId data type
+            // Default value is set to a new ObjectIds
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            // 280 character maximum
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            // Date
+            // Set default value to the current timestamp
+            // Use a getter method to format the timestamp on query
+        },
+    }
+);
+
+
+module.exports = reactionSchema;
