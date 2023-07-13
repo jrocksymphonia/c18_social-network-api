5fa989a4158568f63856bbd2193a4549884efb32
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-07-13 13:00:28 -0400

    set up commented routes for me to prep

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,4 +1,12 @@
 const router = require('express').Router();
 
+// `GET` to get all thoughts
 
+// `GET` to get a single thought by its `_id`
 
+// `POST` to create a new thought 
+// (don't forget to push the created thought's `_id` to the associated user's `thoughts` array field)
+
+// `PUT` to update a thought by its `_id`
+
+// `DELETE` to remove a thought by its `_id`
