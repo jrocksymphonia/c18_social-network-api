e9b14b7521a701c26ada7b9f3a65138ba7f7c3f7
jrocksymphonia <122930110+jrocksymphonia@users.noreply.github.com>
2023-07-16 13:52:03 -0400

    started defining the user controller routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,34 @@
 const { User, Thought } = require('../models');
 
+module.exports = {
+    // Get all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+
+      res.json(users);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // Get a single user
+  async getSingleUser(req, res) {
+    try {
+      const student = await Student.findOne({ _id: req.params.studentId })
+        .select('-__v');
+
+      if (!student) {
+        return res.status(404).json({ message: 'No student with that ID' })
+      }
+
+      res.json({
+        student,
+        grade: await grade(req.params.studentId),
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+}
